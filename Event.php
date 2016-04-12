bff48f318a5d2f333ce509f5d5e3a6b09b0c2d7c
kurozumi <kurozumi@kurozumi.local>
2016-04-12 19:11:17 +0900

    first commit

---
+++ Event.php
@@ -0,0 +1,19 @@
+<?php
+namespace Plugin\CustomEntry;
+
+use Eccube\Event\EventArgs;
+
+class Event
+{
+    /**
+     * @param EventArgs $event
+     */
+    public function onFormInitializeEntry(EventArgs $event)
+    {
+        $builder = $event->getArgument('builder');
+        $builder->add('tel', 'tel', array(
+            'required' => false
+        ));
+    }
+
+}
